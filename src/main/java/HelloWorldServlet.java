1619ba274a7bc88d2df1d8e33ccb4b6ae92ede7f
melodytempleton <mtempletoncantu@gmail.com>
2017-06-06 11:14:38 -0500

    complete name exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,33 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+
+
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
-    }
+    int counter = 0;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            counter++;
+            String aName = request.getParameter("name");
+
+            if(aName != null) {
+                    out.println("<h1>Hello, " + aName + "</h1>");
+
+                }
+
+            else {
+                out.println("<h1>Hello, World!</h1>");
+            }
+        out.println("<h2>Page visit count: " + counter + "</hw>");
+        }
+
 }
