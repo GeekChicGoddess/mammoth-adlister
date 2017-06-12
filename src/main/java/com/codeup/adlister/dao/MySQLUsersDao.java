3344ae130c09d2bed9a403cf309c41d9862490cf
melodytempleton <mtempletoncantu@gmail.com>
2017-06-12 16:08:30 -0500

    Add user Dao Add user Dao to dao factory Add register.jsp file
    still setting up register.jsp

---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,66 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+/**
+ * Created by melodytempleton on 6/12/17.
+ */
+public class MySQLUsersDao implements Users {
+    Connection connection = null;
+
+
+    public MySQLUsersDao (Config config){
+
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to databaser", e);
+        }
+    }
+
+    @Override
+    public User  findByUsername (String username){
+        PreparedStatement statement = null;
+
+        try {
+            String SQL = "SELECT * FROM users WHERE username LIKE ?";
+            statement = connection.prepareStatement(SQL);
+            statement.setString(1, username);
+            ResultSet rs = statement.executeQuery();
+            return new User (rs.getLong(1), rs.getString(2), rs.getString(4), rs.getString(3));
+
+        }
+        catch (SQLException e) {
+            throw new RuntimeException("Error retrieving user", e);
+        }
+    }
+
+    @Override
+    public Long insert (User user){
+        PreparedStatement statement = null;
+        try {
+            String SQL = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+            statement = connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getEmail());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            resultSet.next();
+            return resultSet.getLong(1);
+        }
+        catch (SQLException e) {
+            throw new RuntimeException("Error creating new user", e);
+        }
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/register.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: melodytempleton
+  Date: 6/12/17
+  Time: 4:01 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <jsp:include page="/WEB-INF/partials/head.jsp">
+        <jsp:param name="title" value="New User Sign-up"/>
+    </jsp:include>
+</head>
+<body>
+<jsp:include page="/WEB-INF/partials/navbar.jsp"></jsp:include>
+
+
+
+</body>
+</html>
